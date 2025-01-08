bba3eb59d6afc4664fd60c8e7a98e31b5c491aab
cptnez <gtracey24@gmail.com>
2025-01-08 15:16:30 -0600

    Set up frontedn and backend folders. Added pages, starting with dashboard UI.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.css'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+
+  return (
+    <>
+<Dashboard />
+
+    </>
+  )
+}
+
+export default App
