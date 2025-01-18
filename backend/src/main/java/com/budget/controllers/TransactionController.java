0c4e27eb9de887010b4622c2688e7ba322b26f68
cptnez <gtracey24@gmail.com>
2025-01-17 20:52:18 -0600

    Working on displayTransactions in controller.

--- backend/src/main/java/com/budget/controllers/TransactionController.java
+++ backend/src/main/java/com/budget/controllers/TransactionController.java
@@ -1,10 +1,16 @@
 package com.budget.controllers;
 
+import com.budget.entities.Transaction;
+import com.budget.repos.TransactionRepo;
 import com.budget.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/transactions")
@@ -12,5 +18,21 @@ public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
+
+    @Autowired
+    private TransactionRepo transactionRepo;
+
+    @GetMapping("")
+    public String displayTransactions(@RequestParam(required = false) Long id, Model model) {
+        if(id == null) {
+            model.addAttribute("h3", "Transactions");
+            model.addAttribute("transactions", transactionRepo.findAll());
+        } else {
+            Optional<Transaction> result = transactionRepo.findById(id);
+            if (result.isEmpty()) {
+                model.addAttribute("h3", "Invalid Transaction ID: " + id);
+            }
+        }
+    }
 
 }
