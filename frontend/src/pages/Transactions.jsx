233fdacdd18ae3ecf5b7229b50742541672a7543
cptnez <gtracey24@gmail.com>
2025-01-24 20:06:36 -0600

    Added transaction form, deleted transaction button. TODO: Add button.

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,11 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Form } from "react";
 import axios from "axios";
 
 const Transactions = () => {
+
+
   return (
     <>
     <h1>Transactions</h1>
-    <button type="button" class="btn btn-primary btn-lg btn-block">Add transaction</button>
+    <div class="card m-2">
+    <form>
+     <div class="form-group">
+        <label for="newTransaction" class="card-title">
+        <h4>New Transaction</h4>
+        </label>
+        <input type="text" class="form-control" id="newTransactionDesc" placeholder="Enter Description" />
+        <input type="date" class="form-control" id="newTransactionDate" placeholder="Enter Date" />
+        <input type="number" class="form-control" id="newTransactionAmount" placeholder="Enter Amount" />
+     </div>
+    </form>
+    </div>
       <div class="d-flex p-2">
         <div class="card m-5">
           <div class="card-title">
